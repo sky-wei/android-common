6dccb87706f7b8aeb579abe94b45dfc7a5a19d87
sky <jingcai.wei@163.com>
2018-07-15 19:37:12 +0800

    修改部分类的方法

--- library/src/main/java/com/sky/android/common/util/DisplayUtil.java
+++ library/src/main/java/com/sky/android/common/util/DisplayUtil.java
@@ -21,10 +21,14 @@ import android.content.Context;
 /**
  * Created by sky on 14-12-16.
  */
-public class DisplayUtils {
+public class DisplayUtil {
 
     public static float getDensity(Context context) {
         return context.getResources().getDisplayMetrics().density;
+    }
+
+    public static float getScaledDensity(Context context) {
+        return context.getResources().getDisplayMetrics().scaledDensity;
     }
 
     public static int dip2px(Context context, float dipValue){
@@ -33,6 +37,28 @@ public class DisplayUtils {
 
     public static int px2dip(Context context, float pxValue){
         return px2dip(pxValue, getDensity(context));
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     * @param context
+     * @param pxValue
+     *            （DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        return px2sp(pxValue, getScaledDensity(context));
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     * @param context
+     * @param spValue
+     *            （DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        return sp2px(spValue, getScaledDensity(context));
     }
 
     /**
