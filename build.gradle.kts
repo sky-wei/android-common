d9f57bc8c184af35c5ddf873cac83078f256998c
sky <jingcai.wei@163.com>
2025-07-25 22:18:06 +0800

    调整项目

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+//    alias(libs.plugins.kotlin.compose) apply false
+//    alias(libs.plugins.ksp) apply false
+//    alias(libs.plugins.hilt) apply false
+//    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 pluginManagement {
     repositories {
-        maven { url=uri("https://maven.aliyun.com/repository/public") }
-        maven { url=uri("https://maven.aliyun.com/repository/central") }
-        maven { url=uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url=uri("https://jitpack.io") }
+//        maven { url=uri("https://maven.aliyun.com/repository/public") }
+//        maven { url=uri("https://maven.aliyun.com/repository/central") }
+//        maven { url=uri("https://maven.aliyun.com/repository/gradle-plugin") }
+//        maven { url=uri("https://jitpack.io") }
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -18,9 +18,9 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url=uri("https://maven.aliyun.com/repository/public") }
-        maven { url=uri("https://maven.aliyun.com/repository/central") }
-        maven { url=uri("https://jitpack.io") }
+//        maven { url=uri("https://maven.aliyun.com/repository/public") }
+//        maven { url=uri("https://maven.aliyun.com/repository/central") }
+//        maven { url=uri("https://jitpack.io") }
         google()
         mavenCentral()
     }
